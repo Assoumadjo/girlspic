657480a87afaaace2831cddbd859a0fb88852710
Assoumadjo <damoh.asmaa@gmail.com>
2013-11-24 19:15:08 +0000

    annonce - Taxonomies

--- wp-content/plugins/annonce plugin/annonce-plugin.php
+++ wp-content/plugins/annonce plugin/annonce-plugin.php
@@ -53,7 +53,7 @@ function create_annonce()
 				//'page-attributes', // (menu order, hierarchical must be true to show Parent option)
 				//'post-formats',
 			),
-			//'taxonomies' => array( 'category', 'post_tag' ), // add default post categories and tags
+			'taxonomies' => array( 'category', 'post_tag' ), // add default post categories and tags
 			'menu_position' => 5,
 		//	'register_meta_box_cb' => 'projet_add_post_type_metabox'
 		)
@@ -63,9 +63,17 @@ function create_annonce()
 */
 register_taxonomy( 'annonce_category', // register custom taxonomy - quote category
 			'wp_annonce',
-			array( 'hierarchical' => true,
-				'label' => __( 'Categories des annonces' )
-			)
+			array(
+            'labels' => array(
+                'name' => 'Categories des annonces',
+                'add_new_item' => 'Add New announce category'
+            ),
+            'show_ui' => true,
+            'show_tagcloud' => false,
+            'hierarchical' => true
+        )
+		
+			
 		);
 		register_taxonomy( 'annonce_tag', // register custom taxonomy - quote tag
 			'wp_annonce',
@@ -143,7 +151,7 @@ function annonce_form()
 				//$option .= '<label for="genre">'.$genre->name.'</label>';
 				$option='<li id="download_category-'.$counter.'" class="popular-category">
 				<label class="selectit" >
-				<input value="'.$genre->name.'" type="checkbox" name="category[]" id="'.$genre->name.'">'.$genre->name.'</label></li>';
+				<input value="'.$genre->term_taxonomy_id.'" type="checkbox" name="category[]" id="'.$genre->name.'">'.$genre->name.'</label></li>';
 				echo $option;
 				}
 			?>
@@ -165,8 +173,8 @@ function annonce_form()
 		<input type="text" name="goal" id="goal" value="" placeholder="DD/MM/YY">
 	</p>
 	<p class="atcf-submit-campaign-image">
-		<label for="image">Image de l'annonce</label>
-		<input type="file" name="image" id="image">
+		<label for="thumbnail">Image de l'annonce</label>
+		<input type="file" name="thumbnail" id="thumbnail">
 
 			</p>
 	<p class="atcf-submit-campaign-video">
@@ -219,8 +227,10 @@ function save_annonce_data()
 		exit;
 	}
 	if (isset($_POST['category']) && is_array($_POST['category'])) {
-		foreach($_POST['category'] as $category) echo $category;
-		$tableau=array($_POST['category']);
+		foreach($_POST['category'] as $category) 
+			{
+		echo $category;
+		$tableau=array($_POST['category']);}
 	}
 	else
 	{
@@ -236,6 +246,12 @@ function save_annonce_data()
 		echo "Choose a deadline";
 		exit;
 	}
+	if (isset ($_FILES['thumbnail'])) {
+			$file = $_FILES['thumbnail'];
+		} else {
+			echo 'Photo manquante';
+			exit;
+			}
 	if(isset($_POST['video']))
 	{
 		$video=$_POST['video'];
@@ -246,23 +262,53 @@ function save_annonce_data()
 		exit;
 	}
 
+$category='annonce_category'; // category name for the post
+$cat_ID = get_cat_ID( $category );
 
 	$post = array(
 			'post_title' => wp_strip_all_tags( $title ),
 			'post_content' => $description,
-			 
 			 'description' => $description,
+			'post_category'=>array('8','10'),
 			'post_status' => 'publish',			// Choose: publish, preview, future, etc.
-			'post_type' => $_POST['post-type'] , // Use a custom post type if you want to
-			'post_category'        =>  array( 'genre' => array($_POST['category']) )  // support for custom taxonomies.   
+			'post_type' => $_POST['post-type'] , 
+		//	'tax_input' => array( 'category' => $cat_ID ),
+			//'tax_input'   =>  array( 'category' => array($_POST['category']) ) 
+			// Use a custom post type if you want to
+			//'post_category'  =>  array( 'genre' => array($_POST['category']) )  // support for custom taxonomies.   
 
 		);
 
 		$the_post_id=wp_insert_post($post);  // http://codex.wordpress.org/Function_Reference/wp_insert_post
+		if ($_FILES) {
+	    foreach ($_FILES as $file => $array) {
+	    $newupload = insert_image($file,$the_post_id);
+}
+}
+print_r($tableau);
 		add_post_meta($the_post_id,'url-video',$video);
 		add_post_meta($the_post_id,'deadline',$deadline);
-		wp_set_post_terms($the_post_id,array($_POST['category']),'genre',true);
+		//add_post_meta($the_post_id,'deadline',$tableau);
+		wp_set_post_terms( $the_post_id,$_POST['category'], 'taxonomy_slug');
+		wp_set_post_categories( $the_post_id, $_POST['category'] );
+		//wp_set_post_terms($the_post_id,(array)$_POST['category'],'annonce_category',true);
+		//$the_post_id=wp_insert_post($post);
+	//	wp_set_post_terms($the_post_id,$tableau,'annonce_category',true);
+		//wp_set_post_terms( $the_post_id, (array)$_POST['category'], 'annonce_category', true );
+		// wp_set_post_terms($pid,(array)$_POST['genre'],'gen re',true);
+		//wp_set_post_terms( $post_id, $tag, $taxonomy );
 		
 }
-
+function insert_image($file_handler,$post_id,$setthumb='false') {
+	    // check to make sure its a successful upload
+	    if ($_FILES[$file_handler]['error'] !== UPLOAD_ERR_OK) __return_false();
+	 
+	    require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+	    require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+	    require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+	 
+	    $attach_id = media_handle_upload( $file_handler, $post_id );
+	 	if ($setthumb) update_post_meta($post_id,'_thumbnail_id',$attach_id);
+	    return $attach_id;
+	}
 ?>
